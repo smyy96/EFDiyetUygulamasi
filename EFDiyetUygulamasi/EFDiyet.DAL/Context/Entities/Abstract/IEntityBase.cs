76c02b7c3287dd4962d7cc0c9884e043a439524b
smyy.cskn@hotmail.com <Sumeyye12A>
2024-03-20 00:37:01 +0300

    migration oluşturuldu default degerler eklendi.

--- EFDiyetUygulamasi/EFDiyet.DAL/Context/Entities/Abstract/IEntityBase.cs
+++ EFDiyetUygulamasi/EFDiyet.DAL/Context/Entities/Abstract/IEntityBase.cs
@@ -9,7 +9,7 @@ namespace EFDiyet.DAL.Context.Entities.Abstract
     public interface IEntityBase
     {
         public int Id { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } 
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
