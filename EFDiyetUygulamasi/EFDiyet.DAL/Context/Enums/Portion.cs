c07c2182c236e93931ac29a2c16beaf9dcc8f86e
karahanhivda <hivda.krhn@gmail.com>
2024-03-19 16:49:25 +0300

    Enumlar eklendi.

---
+++ EFDiyetUygulamasi/EFDiyet.DAL/Context/Enums/Portion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDiyet.DAL.Context.Enums
+{
+    public enum Portion
+    {
+        Gram = 1,
+        Glass,
+        Slice,
+        MilliLiter,
+        Liter,
+        Bowl,
+        Floweret,
+        Tablespoon,
+        Unit,
+        Serving,
+        Plate,
+        Block,
+        Piece
+    }
+}
