24d8ce41065c636f7dab5faacbcd830f76d5b2c9
karahanhivda <hivda.krhn@gmail.com>
2024-03-20 18:00:49 +0300

    Manager ve IManager eklendi.

--- EFDiyetUygulamasi/EFDiyet.BLL/Manager/Concrete/CategoryManager.cs
+++ EFDiyetUygulamasi/EFDiyet.BLL/Manager/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
-﻿using EFDiyet.DAL.Context.Entities.Concrete;
+﻿using EFDiyet.BLL.Manager.Abstract;
+using EFDiyet.DAL.Context.Entities.Concrete;
 using EFDiyet.DAL.Repository.Concrete;
 using System;
 using System.Collections.Generic;
