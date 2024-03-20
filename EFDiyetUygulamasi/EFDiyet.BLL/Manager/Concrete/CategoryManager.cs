9ab31e5dcf6e2d7a3981b252b51b138040a50ff7
smyy.cskn@hotmail.com <Sumeyye12A>
2024-03-20 15:31:22 +0300

    Referans verildi. Klasorler olusturuldu.

---
+++ EFDiyetUygulamasi/EFDiyet.BLL/Manager/Concrete/CategoryManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDiyet.BLL.Manager.Concrete
+{
+    internal class CategoryManager
+    {
+    }
+}
