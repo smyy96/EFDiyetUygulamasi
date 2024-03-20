8988cf8100fe1937ba75c80d313664091b386413
smyy.cskn@hotmail.com <Sumeyye12A>
2024-03-20 15:44:13 +0300

    ManagerConcrete

--- EFDiyetUygulamasi/EFDiyet.BLL/Manager/Concrete/UserDetailManager.cs
+++ EFDiyetUygulamasi/EFDiyet.BLL/Manager/Concrete/UserDetailManager.cs
@@ -8,8 +8,7 @@ using System.Threading.Tasks;
 
 namespace EFDiyet.BLL.Manager.Concrete
 {
-    public class CategoryManager : Manager<CategoryModel, Category, CategoryRepository>
+    public class UserDetailManager : Manager<UserDetailModel,UserDetail,UserDetailRepository>
     {
-        
     }
 }
