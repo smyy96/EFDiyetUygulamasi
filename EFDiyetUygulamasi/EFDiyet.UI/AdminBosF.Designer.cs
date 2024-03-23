7faf6dc6a065e2d841af4a7572d2efc948725715
ilginayaz <ilgindeniz.ayaz@bilgeadamboost.com>
2024-03-23 03:34:50 +0300

    admin ve kullanici bos formlar

---
+++ EFDiyetUygulamasi/EFDiyet.UI/AdminBosF.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace formlarg
+{
+    partial class AdminBosF
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminBosF));
+            pbBesinEkle = new PictureBox();
+            pbOgunEkle = new PictureBox();
+            pbAnaliz = new PictureBox();
+            pbCikisAdmin = new PictureBox();
+            lblDietAppA = new Label();
+            lblBesinEkle = new Label();
+            lblOgunEkle = new Label();
+            lblAnalizA = new Label();
+            lblAdmin = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbBesinEkle).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbOgunEkle).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbAnaliz).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbCikisAdmin).BeginInit();
+            SuspendLayout();
+            // 
+            // pbBesinEkle
+            // 
+            pbBesinEkle.BackColor = Color.FromArgb(255, 208, 177);
+            pbBesinEkle.Image = (Image)resources.GetObject("pbBesinEkle.Image");
+            pbBesinEkle.Location = new Point(177, 9);
+            pbBesinEkle.Name = "pbBesinEkle";
+            pbBesinEkle.Size = new Size(43, 37);
+            pbBesinEkle.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbBesinEkle.TabIndex = 0;
+            pbBesinEkle.TabStop = false;
+            // 
+            // pbOgunEkle
+            // 
+            pbOgunEkle.BackColor = Color.FromArgb(255, 208, 177);
+            pbOgunEkle.Image = (Image)resources.GetObject("pbOgunEkle.Image");
+            pbOgunEkle.Location = new Point(333, 9);
+            pbOgunEkle.Name = "pbOgunEkle";
+            pbOgunEkle.Size = new Size(42, 37);
+            pbOgunEkle.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbOgunEkle.TabIndex = 1;
+            pbOgunEkle.TabStop = false;
+            // 
+            // pbAnaliz
+            // 
+            pbAnaliz.BackColor = Color.FromArgb(255, 208, 177);
+            pbAnaliz.Image = (Image)resources.GetObject("pbAnaliz.Image");
+            pbAnaliz.Location = new Point(496, 9);
+            pbAnaliz.Name = "pbAnaliz";
+            pbAnaliz.Size = new Size(45, 37);
+            pbAnaliz.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbAnaliz.TabIndex = 2;
+            pbAnaliz.TabStop = false;
+            // 
+            // pbCikisAdmin
+            // 
+            pbCikisAdmin.BackColor = Color.FromArgb(255, 208, 177);
+            pbCikisAdmin.Image = (Image)resources.GetObject("pbCikisAdmin.Image");
+            pbCikisAdmin.Location = new Point(751, 16);
+            pbCikisAdmin.Name = "pbCikisAdmin";
+            pbCikisAdmin.Size = new Size(37, 30);
+            pbCikisAdmin.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbCikisAdmin.TabIndex = 3;
+            pbCikisAdmin.TabStop = false;
+            // 
+            // lblDietAppA
+            // 
+            lblDietAppA.AutoSize = true;
+            lblDietAppA.BackColor = Color.FromArgb(255, 208, 177);
+            lblDietAppA.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblDietAppA.Location = new Point(22, 9);
+            lblDietAppA.Name = "lblDietAppA";
+            lblDietAppA.Size = new Size(108, 32);
+            lblDietAppA.TabIndex = 4;
+            lblDietAppA.Text = "DietApp";
+            // 
+            // lblBesinEkle
+            // 
+            lblBesinEkle.AutoSize = true;
+            lblBesinEkle.BackColor = Color.FromArgb(255, 208, 177);
+            lblBesinEkle.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblBesinEkle.Location = new Point(226, 18);
+            lblBesinEkle.Name = "lblBesinEkle";
+            lblBesinEkle.Size = new Size(86, 23);
+            lblBesinEkle.TabIndex = 5;
+            lblBesinEkle.Text = "Besin Ekle";
+            // 
+            // lblOgunEkle
+            // 
+            lblOgunEkle.AutoSize = true;
+            lblOgunEkle.BackColor = Color.FromArgb(255, 208, 177);
+            lblOgunEkle.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblOgunEkle.Location = new Point(381, 18);
+            lblOgunEkle.Name = "lblOgunEkle";
+            lblOgunEkle.Size = new Size(89, 23);
+            lblOgunEkle.TabIndex = 6;
+            lblOgunEkle.Text = "Öğün Ekle";
+            // 
+            // lblAnalizA
+            // 
+            lblAnalizA.AutoSize = true;
+            lblAnalizA.BackColor = Color.FromArgb(255, 208, 177);
+            lblAnalizA.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblAnalizA.Location = new Point(560, 18);
+            lblAnalizA.Name = "lblAnalizA";
+            lblAnalizA.Size = new Size(56, 23);
+            lblAnalizA.TabIndex = 7;
+            lblAnalizA.Text = "Analiz";
+            // 
+            // lblAdmin
+            // 
+            lblAdmin.AutoSize = true;
+            lblAdmin.BackColor = Color.FromArgb(255, 208, 177);
+            lblAdmin.Font = new Font("Segoe UI Semibold", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblAdmin.Location = new Point(649, 18);
+            lblAdmin.Name = "lblAdmin";
+            lblAdmin.Size = new Size(60, 23);
+            lblAdmin.TabIndex = 8;
+            lblAdmin.Text = "Admin";
+            // 
+            // AdminBosF
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblAdmin);
+            Controls.Add(lblAnalizA);
+            Controls.Add(lblOgunEkle);
+            Controls.Add(lblBesinEkle);
+            Controls.Add(lblDietAppA);
+            Controls.Add(pbCikisAdmin);
+            Controls.Add(pbAnaliz);
+            Controls.Add(pbOgunEkle);
+            Controls.Add(pbBesinEkle);
+            DoubleBuffered = true;
+            Name = "AdminBosF";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pbBesinEkle).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbOgunEkle).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbAnaliz).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbCikisAdmin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pbBesinEkle;
+        private PictureBox pbOgunEkle;
+        private PictureBox pbAnaliz;
+        private PictureBox pbCikisAdmin;
+        private Label lblDietAppA;
+        private Label lblBesinEkle;
+        private Label lblOgunEkle;
+        private Label lblAnalizA;
+        private Label lblAdmin;
+    }
+}
