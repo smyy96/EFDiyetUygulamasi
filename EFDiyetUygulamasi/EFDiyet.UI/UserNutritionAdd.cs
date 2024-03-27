5ee9498b0cdf4bff25f7a92ce1b4d7de363338f3
karahanhivda <hivda.krhn@gmail.com>
2024-03-27 21:36:21 +0300

    BesinEkleme eklendi

--- EFDiyetUygulamasi/EFDiyet.UI/UserNutritionAdd.cs
+++ EFDiyetUygulamasi/EFDiyet.UI/UserNutritionAdd.cs
@@ -1,4 +1,5 @@
-﻿using EFDiyet.BLL.Model;
+﻿using EFDiyet.BLL.Manager.Concrete;
+using EFDiyet.BLL.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,16 +19,77 @@ namespace EFDiyet.UI
         {
             InitializeComponent();
             this.GetUser = userModel;
+            InitializeComponent();
+
         }
+
+        private void FormClear()
+        {
+            txt_NutriValue.Text = string.Empty;
+            txt_Calorie.Text = string.Empty;
+            txt_Portion.Text = string.Empty;
+            txt_PortionSize.Text = string.Empty;
+            txt_NutriName.Text = string.Empty;
+            txt_NutriCCategory.Text = string.Empty;
+            cmb_Meals.SelectedIndex = -1;
+            txt_BesinAdeti.Text = string.Empty;
+        }
+
+
 
         private void UserNutritionAdd_Load(object sender, EventArgs e)
         {
-
+            NutritionManager nutritionManager = new NutritionManager();
+            var data = nutritionManager.GetNutritionDetails()
+                .Select(n => new List<DAL.Context.Entities.Concrete.Nutrition> { });       
+                    
+            dtg_Nutrition.DataSource = nutritionManager.GetNutritionDetails().ToList(); 
         }
 
         private void btn_NutritionAdd_Click(object sender, EventArgs e)
         {
+            if (dtg_Nutrition.SelectedRows.Count > 0)
+            {
+                int selectedRowIndex = dtg_Nutrition.SelectedCells[0].RowIndex;
+                int selectedEntityId = (int)dtg_Nutrition.Rows[selectedRowIndex].Cells["Id"].Value;
 
+                NutritionManager nutritionManager = new NutritionManager();
+                var selectedNutrition = nutritionManager.GetById(selectedEntityId);
+
+
+                UserNutritionManager userNutritionManager = new UserNutritionManager();
+                UserNutritionModel model = new UserNutritionModel();
+                model.UserId = GetUser.Id;
+                model.NutritionId = dtg_Nutrition_DoubleClick.Entity.NutritionId;
+
+
+
+                model.NutritionId = selectedNutrition.Id;
+                userNutritionManager.Insert(model);
+
+                MessageBox.Show("Seçilen besin başarıyla eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir besin seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        
         }
+
+        private void dtg_Nutrition_DoubleClick(object sender, EventArgs e)
+        {
+            NutritionManager nutritionManager = new NutritionManager();
+            var data = nutritionManager.GetNutritionDetails()
+                .Select(n => new
+                {
+                    txt_NutriName = n.NutritionName,
+                    txt_Calorie = n.Calories,
+                    txt_NutriCCategory = n.Category.CategoryName,
+                    txt_NutriValue = n.NutritionValue.NutritionValueName,
+                    txt_Portion = n.Portion,
+                    txt_PortionSize = n.PortionSize
+                }).ToList();
+        
+    }
     }
 }
