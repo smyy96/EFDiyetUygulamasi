6718de59b742cda39f6939a57ba34f2dd2f7b201
smyy.cskn@hotmail.com <Sumeyye12A>
2024-03-27 22:28:57 +0300

    acıklama

--- EFDiyetUygulamasi/EFDiyet.UI/Program.cs
+++ EFDiyetUygulamasi/EFDiyet.UI/Program.cs
@@ -20,11 +20,10 @@ namespace EFDiyet.UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
+            //Application.Run(new LoginForm());
             //Application.Run(new BesinEkleAdmin());
             //Application.Run(new AdminPanel());
-            //Application.Run(new UserForm());
-            //Application.Run(new UserForm(GetUser));
+            Application.Run(new UserForm(GetUser));
         }
     }
 }
